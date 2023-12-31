a1a0f1c8d9290e807a0bbb5274e8be8fda5a3f8c
garry_host <huangqiannb@gmail.com>
2023-12-31 22:40:22 +0800

    User authenticated with flask-login

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import sys
 import os
 from flask import Flask, render_template, redirect, url_for, flash, request
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 import click
+from flask_login import LoginManager,UserMixin,login_user,logout_user,login_required, current_user
+
 
 app = Flask(__name__)
 WIN = sys.platform.startswith('win')
@@ -15,7 +18,29 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的
 app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
+
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+def admin(username, password):
+    """Create user."""
+    db.create_all()
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)
+        db.session.add(user)
+    db.session.commit()
+    click.echo('Done.')
 
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop.')
@@ -34,36 +59,47 @@ def forge():
     # 全局的两个变量移动到这个函数内
     name = 'Garry HOst'
     movies = [
-        {'title': 'My Neighbor Totoro', 'year': '1988'},
-        {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mahjong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
+        {'title': '龙猫-My Neighbor Totoro', 'year': '1988','poster':'https://img9.doubanio.com/view/photo/l/public/p2540924496.webp'},
+        {'title': '死亡诗社-Dead Poets Society', 'year': '1989','poster':'https://img1.doubanio.com/view/photo/m/public/p2575465690.webp'},
+        {'title': '完美的世界-A Perfect World', 'year': '1993','poster':'https://img1.doubanio.com/view/photo/m/public/p1562273940.webp'},
+        {'title': '这个杀手不太冷-Leon', 'year': '1994','poster':'https://img3.doubanio.com/view/photo/m/public/p2408302602.webp'},
+        {'title': '麻将-Mahjong', 'year': '1996','poster':'https://img9.doubanio.com/view/photo/m/public/p2873829595.webp'},
+        {'title': '燕尾蝶-Swallowtail Butterfly', 'year': '1996','poster':'https://img3.doubanio.com/view/photo/m/public/p2056451383.webp'},
+        {'title': '喜剧之王-King of Comedy', 'year': '1999','poster':'https://img1.doubanio.com/view/photo/m/public/p2181431029.webp'},
+        {'title': '鬼子来了-Devils on the Doorstep', 'year': '1999','poster':'https://img1.doubanio.com/view/photo/m/public/p2553104888.webp'},
+        {'title': '机器人总动员-WALL-E', 'year': '2008','poster':'https://img1.doubanio.com/view/photo/m/public/p1037415760.webp'},
+        {'title': '麦兜-The Pork of Music', 'year': '2012','poster':'https://img9.doubanio.com/view/photo/m/public/p1626461596.webp'},
     ]
     
     user = User(name=name)
     db.session.add(user)
     for m in movies:
-        movie = Movie(title=m['title'], year=m['year'])
+        movie = Movie(title=m['title'], year=m['year'], poster=m['poster'])
         db.session.add(movie)
 
     db.session.commit()
     click.echo('Done.')
 # ...
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True) # primary key
     name = db.Column(db.String(20)) # name
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):         
+        self.password_hash = generate_password_hash(password)
+    
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary key
     title = db.Column(db.String(60))    # movie title
     year = db.Column(db.String(4))  # movie year
+    poster = db.Column(db.String(256))
+
 
 
 @app.context_processor
@@ -87,6 +123,9 @@ def bad_request(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
+        
         title = request.form.get('title')
         year = request.form.get('year')
         
@@ -105,6 +144,7 @@ def index():
 
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
 
@@ -126,6 +166,7 @@ def edit(movie_id):
 
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
@@ -134,5 +175,61 @@ def delete(movie_id):
     return redirect(url_for('index'))
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username =request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+        # 验证用户名和密码是否一致
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash('Login success.')
+            return redirect(url_for('index'))
+
+        flash('Invalid username or password.')
+        return redirect(url_for('login'))
+    
+    return render_template('login.html')
 
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Goodbye.')
+    return redirect(url_for('index'))
+
+
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if not name or len(name) > 20:
+            flash('Invalid input.')
+            return redirect(url_for('settings'))
+        current_user.name = name
+        db.session.commit()
+        flash('Settings updated.')
+        return redirect(url_for('index'))
+    
+    return render_template('settings.html')
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+
+
+if __name__ == '__main__':
+    app.run()
